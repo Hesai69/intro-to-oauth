042c0fad8ac36d0ec111f00c3b133cbf0fb66953
Qian Chen <oda_san@hotmail.com>
2017-01-30 14:04:34 -0800

    oauth to github

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/login', (req, res, next) => {
+  res.redirect('https://github.com/login/oauth/authorize');
+});
+
 module.exports = router;
